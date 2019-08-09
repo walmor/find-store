05aca3c663f770144875e94e17f0256a0b08b12b
Walmor Moreira <walmorcosta@gmail.com>
2019-08-09 19:32:41 -0300

    Fix tests that were not running without the configuraton file

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,6 @@ dotenv.config({ path: path.join(__dirname, '.env') });
 
 export const config = {
   PORT: parseInt(process.env.PORT, 10) || 3000,
-  HERE_APP_ID: process.env.HERE_APP_ID,
-  HERE_APP_CODE: process.env.HERE_APP_CODE,
+  HERE_APP_ID: process.env.HERE_APP_ID || '',
+  HERE_APP_CODE: process.env.HERE_APP_CODE || '',
 };
